e9230837d804551c8d5918139e61de81da131329
Tim <tim.englund@elev.ga.ntig.se>
2023-11-08 10:10:02 +0100

    uppgift b klar

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Uppgift a)");
-        int i = 0;
+        int i = 2;
         while(i<=5){
             System.out.println(i);
             i++;
