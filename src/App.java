1dacaf2b69ab65d8ef53e3166c451054bfb6ef19
Tim <tim.englund@elev.ga.ntig.se>
2023-11-08 10:43:15 +0100

    uppgift g klar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
         System.out.println("Uppgift a+b)");
         int i = 2;
         while(i<=5){
@@ -29,5 +32,17 @@ public class App {
         System.out.println("Uppgift f)");
         for(int i5=5; i5>=0; i5--)
             System.out.println(i5);
+        
+        System.out.println(" ");
+        System.out.println("Uppgift g)");
+        
+        int[] tal = new int[5];
+        for(int i6=0; i6<tal.length; i6++){
+            System.out.println("vad är ditt tal för ["+i6+"]");
+            tal[i6] = in.nextInt();
+        }
+        for(int i7=0; i7<tal.length; i7++){
+            System.out.println("Cell med index "+i7+" har värdet: "+tal[i7]);
+        }
     }
 }
